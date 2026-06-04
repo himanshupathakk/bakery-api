e3e1806ea293fdae020a8d77bceafc5d9443ca18
himanshupathakk <textforhimanshu@gmail.com>
2026-06-04 16:44:57 +0200

    Prepare for Neon and Render

--- app.py
+++ app.py
@@ -40,7 +40,9 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
     app.config["OPENAPI_SWAGGER_UI_URL"]  = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
 
-    app.config["SQLALCHEMY_DATABASE_URI"]        = db_url or os.getenv("DATABASE_URL", "sqlite:///bakery.db")
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+    db_url or os.getenv("DATABASE_URL", "sqlite:///bakery.db")
+)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     app.config["JWT_SECRET_KEY"]           = os.getenv("JWT_SECRET_KEY", "bakery-secret-key-change-in-prod")
